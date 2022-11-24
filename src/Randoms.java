da39255238d781a889b9f7d1489b6ed801a6832c
Anastasia <hagnum@mail.ru>
2022-11-24 14:54:18 +0500

    new file

---
+++ src/Randoms.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min;
+    protected int max;
+
+    public Randoms(int min, int max) {
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+    public int generationInt() {
+        return (int) (Math.random() * ((max - min) + 1)) + min;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return Randoms.this.generationInt();
+            }
+        };
+    }
+}
+
